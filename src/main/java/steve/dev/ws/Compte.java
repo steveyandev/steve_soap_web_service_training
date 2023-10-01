f4afc952e71b55c85b966915bccf11b6a8849963
steve desire <stevedesiredev@gmail.com>
2023-10-01 16:33:40 +0200

    add of some JAxB annotation to bind or java classes and object to the good XML way we want

--- src/main/java/steve/dev/ws/Compte.java
+++ src/main/java/steve/dev/ws/Compte.java
@@ -1,10 +1,17 @@
 package steve.dev.ws;
 
-import java.util.Date;
+import jakarta.xml.bind.annotation.XmlAccessType;
+import jakarta.xml.bind.annotation.XmlAccessorType;
+import jakarta.xml.bind.annotation.XmlRootElement;
+import jakarta.xml.bind.annotation.XmlTransient;
 
+import java.util.Date;
+@XmlRootElement(name="compte")
+@XmlAccessorType(XmlAccessType.FIELD)
 public class Compte {
     private  int code;
     private double solde;
+    @XmlTransient
     private Date dateCreation;
 
     public Compte() {
